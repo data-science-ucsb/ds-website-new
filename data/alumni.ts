e55096e92a638f46b752cf0827b75fccd1fa7302
WinbertZhang <winbertzhang@gmail.com>
2023-12-24 04:11:04 -0800

    Update Events

--- data/alumni.ts
+++ data/alumni.ts
@@ -1,124 +1,144 @@
 export const alumniData = [
-    {
-      name: 'Luke Fields',
-      gradYear: 'UCSB \'23',
-      description: 'Luke studied Statistics and Data Science at UCSB and is now a Pricing Analyst at Honda.',
-      imageUrl: '/images/alumni/luke-fields.jpg',
-    },
-    {
-      name: 'Stella Jia',
-      gradYear: 'UCSB \'23',
-      description: 'Stella studied data science at UCSB and is now a data science major at UC Berkeley.',
-      imageUrl: '/images/alumni/stella-jia.jpg',
-    },
-    {
-      name: 'Anna Bauer',
-      gradYear: 'UCSB \'23',
-      description: 'Anna was a Statistics & Data Science major at UCSB and is now a tech analyst at Deloitte.',
-      imageUrl: '/images/alumni/anna-bauer.png',
-    },
-    {
-      name: 'Katie Huynh',
-      gradYear: 'UCSB \'23',
-      description: 'Katie studied Statistics and Data Science at UCSB and is now an analyst at General Mills.',
-      imageUrl: '/images/alumni/katie-huynh.jpg',
-    },
-    {
-      name: 'Connor Levenson',
-      gradYear: 'UCSB \'22',
-      description: 'Connor studied actuarial science at UCSB and is now a research engineer at UCSB.',
-      imageUrl: '/images/alumni/connor-levenson.png',
-    },
-    {
-      name: 'Ishana Narayanan',
-      gradYear: 'UCSB \'22',
-      description: 'Ishana was a Computer Science and Statistics & Data Science double major at UCSB and is now a software engineer at Google.',
-      imageUrl: '/images/alumni/ishana-narayanan.jpg',
-    },
-    {
-      name: 'Brian Lim',
-      gradYear: 'UCSB \'21',
-      description: 'Brian studied computer science at UCSB and is now a software development engineer at Amazon.',
-      imageUrl: '/images/alumni/brian-lim.jpg',
-    },
-    {
-      name: 'Natalie Rozak',
-      gradYear: 'UCSB \'20',
-      description: 'Natalie studied statistics & data science at UCSB and is now a data scientist at BASF.',
-      imageUrl: '/images/alumni/natalie-rozak.jpg',
-    },
-    {
-      name: 'Jasmine Kellogg',
-      gradYear: 'UCSB \'20',
-      description: 'Jasmine studied statistics & data science at UCSB and is now a data analyst at Scalable Commerce.',
-      imageUrl: '/images/alumni/jasmine-kellogg.jpg',
-    },
-    {
-      name: 'Claire Qian',
-      gradYear: 'UCSB \'20',
-      description: 'Claire studied statistics & data science at UCSB and is now a data analyst at Neo Media World.',
-      imageUrl: '/images/alumni/claire-qian.jpg',
-    },
-    {
-      name: 'Sunny Hong',
-      gradYear: 'UCSB \'20',
-      description: 'Sunny studied statistics & data science at UCSB and is now an analyst at RPA.',
-      imageUrl: '/images/alumni/sunny-hong.jpg',
-    },
-    {
-      name: 'Daniel Fields',
-      gradYear: 'UCSB \'19',
-      description: 'Daniel studied statistics at UCSB and is now a data scientist at Pacific Life.',
-      imageUrl: '/images/alumni/daniel-fields.jpg',
-    },
-    {
-      name: 'Shakuntala Mitra',
-      gradYear: 'UCSB \'19',
-      description: 'Shakuntala studied biochemistry at UCSB and is now a ML engineer at AWE Technologies.',
-      imageUrl: '/images/alumni/shakuntala-mitra.jpg',
-    },
-    {
-      name: 'Calvin Wang',
-      gradYear: 'UCSB \'19',
-      description: 'Calvin studied computing at UCSB (CCS) and is now an applied scientist at AWS.',
-      imageUrl: '/images/alumni/calvin-wang.jpg',
-    },
-    {
-      name: 'Jason Freeberg',
-      gradYear: 'UCSB \'18',
-      description: 'Jason studied statistics at UCSB and is now a product manager at Microsoft.',
-      imageUrl: '/images/alumni/jason-freeberg.jpg',
-    },
-    {
-      name: 'Nathan Fritter',
-      gradYear: 'UCSB \'17',
-      description: 'Nathan studied applied statistics at UCSB and is now a data consultant at Qbiz.',
-      imageUrl: '/images/alumni/nathan-fritter.jpg',
-    },
-    {
-      name: 'Celine Mol',
-      gradYear: 'UCSB \'17',
-      description: 'Celine studied statistical sciences at UCSB and is now the Cofounder and CTO of Beni.',
-      imageUrl: '/images/alumni/celine-mol.jpg',
-    },
-    {
-      name: 'Raul Eulogio',
-      gradYear: 'UCSB \'16',
-      description: 'Raul studied statistics at UCSB and is now a Senior Data Science Engineer at Evidation.',
-      imageUrl: '/images/alumni/raul-eulogio.jpg',
-    },
-    {
-      name: 'David Campos',
-      gradYear: 'UCSB \'15',
-      description: 'David studied econ and Spanish at UCSB and is now a data architect at Delta Emerald Ventures.',
-      imageUrl: '/images/alumni/david-campos.jpg',
-    },
-    {
-      name: 'Holly Cheek',
-      gradYear: 'UC Berkeley \'20',
-      description: 'Holly was an operations officer for our club and is now a product manager at Workday.',
-      imageUrl: '/images/alumni/holly-cheek.jpg',
-    },
-  ];
+  {
+    name: "Luke Fields",
+    gradYear: "UCSB '23",
+    description:
+      "Luke studied Statistics and Data Science at UCSB and is now a Pricing Analyst at Honda.",
+    imageUrl: "/images/alumni/luke-fields.jpg",
+  },
+  {
+    name: "Stella Jia",
+    gradYear: "UCSB '23",
+    description:
+      "Stella studied data science at UCSB and is now a data science major at UC Berkeley.",
+    imageUrl: "/images/alumni/stella-jia.jpg",
+  },
+  {
+    name: "Anna Bauer",
+    gradYear: "UCSB '23",
+    description:
+      "Anna was a Statistics & Data Science major at UCSB and is now a tech analyst at Deloitte.",
+    imageUrl: "/images/alumni/anna-bauer.png",
+  },
+  {
+    name: "Katie Huynh",
+    gradYear: "UCSB '23",
+    description:
+      "Katie studied Statistics and Data Science at UCSB and is now an analyst at General Mills.",
+    imageUrl: "/images/alumni/katie-huynh.jpg",
+  },
+  {
+    name: "Connor Levenson",
+    gradYear: "UCSB '22",
+    description:
+      "Connor studied actuarial science at UCSB and is now a research engineer at UCSB.",
+    imageUrl: "/images/alumni/connor-levenson.png",
+  },
+  {
+    name: "Ishana Narayanan",
+    gradYear: "UCSB '22",
+    description:
+      "Ishana was a Computer Science and Statistics & Data Science double major at UCSB and is now a software engineer at Google.",
+    imageUrl: "/images/alumni/ishana-narayanan.jpg",
+  },
+  {
+    name: "Brian Lim",
+    gradYear: "UCSB '21",
+    description:
+      "Brian studied computer science at UCSB and is now a software development engineer at Amazon.",
+    imageUrl: "/images/alumni/brian-lim.jpg",
+  },
+  {
+    name: "Natalie Rozak",
+    gradYear: "UCSB '20",
+    description:
+      "Natalie studied statistics & data science at UCSB and is now a data scientist at BASF.",
+    imageUrl: "/images/alumni/natalie-rozak.jpg",
+  },
+  {
+    name: "Jasmine Kellogg",
+    gradYear: "UCSB '20",
+    description:
+      "Jasmine studied statistics & data science at UCSB and is now a data analyst at Scalable Commerce.",
+    imageUrl: "/images/alumni/jasmine-kellogg.jpg",
+  },
+  {
+    name: "Claire Qian",
+    gradYear: "UCSB '20",
+    description:
+      "Claire studied statistics & data science at UCSB and is now a data analyst at Neo Media World.",
+    imageUrl: "/images/alumni/claire-qian.jpg",
+  },
+  {
+    name: "Sunny Hong",
+    gradYear: "UCSB '20",
+    description:
+      "Sunny studied statistics & data science at UCSB and is now an analyst at RPA.",
+    imageUrl: "/images/alumni/sunny-hong.jpg",
+  },
+  {
+    name: "Daniel Fields",
+    gradYear: "UCSB '19",
+    description:
+      "Daniel studied statistics at UCSB and is now a data scientist at Pacific Life.",
+    imageUrl: "/images/alumni/daniel-fields.jpg",
+  },
+  {
+    name: "Shakuntala Mitra",
+    gradYear: "UCSB '19",
+    description:
+      "Shakuntala studied biochemistry at UCSB and is now a ML engineer at AWE Technologies.",
+    imageUrl: "/images/alumni/shakuntala-mitra.jpg",
+  },
+  {
+    name: "Calvin Wang",
+    gradYear: "UCSB '19",
+    description:
+      "Calvin studied computing at UCSB (CCS) and is now an applied scientist at AWS.",
+    imageUrl: "/images/alumni/calvin-wang.jpg",
+  },
+  {
+    name: "Jason Freeberg",
+    gradYear: "UCSB '18",
+    description:
+      "Jason studied statistics at UCSB and is now a product manager at Microsoft.",
+    imageUrl: "/images/alumni/jason-freeberg.jpg",
+  },
+  {
+    name: "Nathan Fritter",
+    gradYear: "UCSB '17",
+    description:
+      "Nathan studied applied statistics at UCSB and is now a data consultant at Qbiz.",
+    imageUrl: "/images/alumni/nathan-fritter.jpg",
+  },
+  {
+    name: "Celine Mol",
+    gradYear: "UCSB '17",
+    description:
+      "Celine studied statistical sciences at UCSB and is now the Cofounder and CTO of Beni.",
+    imageUrl: "/images/alumni/celine-mol.jpg",
+  },
+  {
+    name: "Raul Eulogio",
+    gradYear: "UCSB '16",
+    description:
+      "Raul studied statistics at UCSB and is now a Senior Data Science Engineer at Evidation.",
+    imageUrl: "/images/alumni/raul-eulogio.jpg",
+  },
+  {
+    name: "David Campos",
+    gradYear: "UCSB '15",
+    description:
+      "David studied econ and Spanish at UCSB and is now a data architect at Delta Emerald Ventures.",
+    imageUrl: "/images/alumni/david-campos.jpg",
+  },
+  {
+    name: "Holly Cheek",
+    gradYear: "UC Berkeley '20",
+    description:
+      "Holly was an operations officer for our club and is now a product manager at Workday.",
+    imageUrl: "/images/alumni/holly-cheek.jpg",
+  },
+];
 
 export default alumniData;
--- data/homepage-events.ts
+++ data/homepage-events.ts
@@ -1,28 +1,32 @@
 const homepageEvents = [
   {
     title: "Fall 2023 Data Science UCSB Career Fair",
-    description: "We hosted our second annual Data Science UCSB Career Fair over Zoom!",
+    description:
+      "We hosted our second annual Data Science UCSB Career Fair over Zoom!",
     imageUrl: "/images/careerfair23/2023-career-fair.jpeg",
   },
   {
     title: "2023 Data Science UCSB Project Showcase",
-    description: "We are hosting our 2023 Project Showcase on April 20, 2023 at Corwin Pavilion, where students will get the chance to present the projects they have been working on all year! We would also like to give a special thank you to our sponsors: Evidation, HRL Laboratories, Adstra, and Microsoft.",
-    imageUrl: "/images/blog2023-24/f23-welcome-social.png",
+    description:
+      "We are hosting our 2023 Project Showcase on April 20, 2023 at Corwin Pavilion, where students will get the chance to present the projects they have been working on all year! We would also like to give a special thank you to our sponsors: Evidation, HRL Laboratories, Adstra, and Microsoft.",
+    imageUrl: "/images/showcase2023/2023-project-showcase-cover.png",
   },
   {
-    title: "Date-A Science 2023",
-    description: "Speed dating event for our new members!",
-    imageUrl: "/images/date-a-science-2023.png",
+    title: "Meeting Times and Locations",
+    description: "TBD",
+    imageUrl: "/images/kickoff.jpg",
   },
   {
-    title: "Alumni August 2023",
-    description: "Weekly panel of UCSB alumni sharing their experiences with data science before and after graduating!",
-    imageUrl: "/images/alumni-august-2023-ss0.png",
+    title: "Workshops",
+    description:
+      "Join us for our weekly workshops on a variety of topics, including Python, R, machine learning, and more!",
+    imageUrl: "/images/home3.png",
   },
   {
-    title: "Alumni August 2023",
-    description: "Weekly panel of UCSB alumni sharing their experiences with data science before and after graduating!",
-    imageUrl: "/images/alumni-august-2023-ss0.png",
+    title: "Resources",
+    description:
+      "Check out our resources page for more information on Data Science at UCSB!",
+    imageUrl: "/images/blog19kickoff.png",
   },
 ];
 
