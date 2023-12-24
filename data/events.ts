574b93036ec8433d893e269619e0a32584366da4
WinbertZhang <winbertzhang@gmail.com>
2023-12-24 04:34:36 -0800

    Add all events

--- data/events.ts
+++ data/events.ts
@@ -27,11 +27,37 @@ const events = [
     imageUrl: "/images/alumni-august-2023-ss0.png",
   },
   {
-    title: "Alumni August 2023",
+    title: "Ice Skating Social 2023",
     description:
-      "Weekly panel of UCSB alumni sharing their experiences with data science before and after graduating!",
-    date: "August 21, 2023",
-    imageUrl: "/images/alumni-august-2023-ss0.png",
+      "Our end-of-the-year social at the Ice in Paradise skating rink!",
+    date: "May 16, 2023",
+    imageUrl: "/images/blog2022-23/ice-skate-cover.jpg",
+  },
+  {
+    title: "2023 Project Showcase",
+    description: "The 2023 Data Science UCSB Project Showcase was a success!",
+    date: "April 20, 2023",
+    imageUrl: "/images/blog2022-23/ds_ps_cover.jpg",
+  },
+  {
+    title: "2022 Big Little Family Reveal",
+    description:
+      "Our Big Little program kicked off with the Family Reveal and some friendly competition!",
+    date: "November 04, 2022",
+    imageUrl: "/images/blog2022-23/f22-big-little-family-reveal-thumbnail.jpg",
+  },
+  {
+    title: "Fall Pizza Social",
+    description: "Our first social of the school year!",
+    date: "October 14, 2022",
+    imageUrl: "/images/blog2022-23/f22-pizza-social-cover.jpg",
+  },
+  {
+    title: "2022 Data Science UCSB Kickoff",
+    description:
+      "Data Science UCSB's kickoff meeting for the 2022-23 school year in Chem 1179!",
+    date: "October 06, 2022",
+    imageUrl: "/images/blog2022-23/2022-kickoff-fall.jpg",
   },
 ];
 
