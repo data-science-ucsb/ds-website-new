46af86d6b071cf940208a7551a72b007f748ce93
iTakedown27 <ezrabridger27@gmail.com>
2024-03-19 17:39:59 -0700

    Added minimal CSS for cards
    Need to expand to whole page on click

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -15,62 +15,37 @@ interface Event {
 
 const imageHeight = 200;
 
-// TODO: Add CSS to expand card and replace imgae with expandedImage and description with longDescription
-const EventCard = ({ title, description, longDescription, date, imageUrl, expandedImage }: Event) => (
-  <div className="event-card">
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-      <div className="relative w-full" style={{ height: `${imageHeight}px` }}>
+function EventCard({ title, description, longDescription, date, imageUrl, expandedImage }: Event) {
+  const [eventInfo, setEventExpanded] = useState(false);
+
+  return (
+    <button onClick={()=>setEventExpanded(!eventInfo)} className="hover:shadow-lg">
+    <motion.div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+      <motion.div className="relative w-full" style={{ height: `${imageHeight}px` }}>
         <Image
-          src={imageUrl}
+          src={(!eventInfo) ? imageUrl : expandedImage}
           alt={title}
           sizes="(max-width: 640px) 100vw, 640px"
           fill
           style={{ objectFit: "cover" }}
           className="bg-coverrounded-t-lg"
         />
-      </div>
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-gray-700 text-base">{description}</p>
-      </div>
-      <div className="px-6 pt-4 pb-2">
+      </motion.div>
+      <motion.div className="px-6 py-4">
+        <motion.div className="font-bold text-xl mb-2">{title}</motion.div>
+        {(!eventInfo) 
+        ? <p className="text-gray-700 text-base">{description}</p>
+        : <p className="text-gray-700 text-base" style={{height: "auto"}}>{longDescription}</p>
+        }
+      </motion.div>
+      <motion.div className="px-6 pt-4 pb-2">
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
           {date}
         </span>
-      </div>
-    </div>
-  </div>
-);
-
-// NON-LAMBDA FUNCTION: This is for useState if needed
-// function EventCard({ title, description, date, imageUrl }: Event) {
-//   const [eventInfo, setEventExpanded] = useState(false);
-
-//   return (
-//     <button onClick={()=>setEventExpanded(!eventInfo)} className="event-card">
-//     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-//       <div className="relative w-full" style={{ height: `${imageHeight}px` }}>
-//         <Image
-//           src={imageUrl}
-//           alt={title}
-//           sizes="(max-width: 640px) 100vw, 640px"
-//           fill
-//           style={{ objectFit: "cover" }}
-//           className="bg-coverrounded-t-lg"
-//         />
-//       </div>
-//       <div className="px-6 py-4">
-//         <div className="font-bold text-xl mb-2">{title}</div>
-//         <p className="text-gray-700 text-base">{description}</p>
-//       </div>
-//       <div className="px-6 pt-4 pb-2">
-//         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-//           {date}
-//         </span>
-//       </div>
-//     </div>
-//   </button>
-//   );
-// }
+      </motion.div>
+    </motion.div>
+  </button>
+  );
+}
 
 export default EventCard;
