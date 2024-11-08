9696981419d098b32ee6ba865c56ad1f7789854d
Clement Boiteux <67805925+cboiteux2765@users.noreply.github.com>
2024-11-07 23:41:04 -0800

    Added recent events (#20)
    * Added recent events

--- app/data/events.ts
+++ app/data/events.ts
@@ -2,6 +2,24 @@ import { description } from "./siteMetadata";
 
 const events = [
   {
+    title: "Undergrad Research Panel",
+    description: "We held an undergraduate research panel featuring Daniel Naylor, Pariya Akhiani, Jiaxin Su, and Henry McMahon! They shared very valuable advice for getting into research!",
+    date: "November 5, 2024",
+    imageUrl: "/images/blog2024-25/research-panel.jpg"
+  },
+  {
+    title: "Fall 2024 Data Science UCSB Career Fair",
+    description: "We hosted our 3rd annual Data Science UCSB Career Fair!",
+    date: "October 25, 2024",
+    imageUrl: "/images/careerfair24/2024-career-fair-logo.png",
+  },
+  {
+    title: "Data Science Band Party!!",
+    description: "We invited The Radar, Dawn Patrol, DMLW, and Orange Pit to play some fire music for us and bond with our spooky Halloween costumes!",
+    date: "October 18, 2024",
+    imageUrl: "/images/blog2024-25/halloween-band-party.jpg"
+  },
+  {
     title: "Intern Panel",
     description: "We hosted a panel featuring Tanay Biradar, Edizon Chang, Sarah Lew, Irena Wong, and Anu Maddali to talk about their internship experience!",
     date: "October 15, 2024",
@@ -42,13 +60,6 @@ const events = [
     description: "We hosted our annual Alumni August event where they shared valuable career experience!",
     date: "August 22, 2024",
     imageUrl: "/images/blog2024-25/alumni-august-2024.png",
-  },
-  {
-    title: "Fall 2024 Data Science UCSB Career Fair",
-    description: "We're going to host our 3rd annual Data Science UCSB Career Fair!",
-    date: "October 25, 2024",
-    imageUrl: "/images/careerfair24/2024-career-fair-logo.png",
-    expandedImage: "/images/careerfair24/2024-career-fair-logo.png"
   },
 ];
 
