7c056e073951efe2c1a169b7bc3f7a5cc032ff31
Clement Boiteux <67805925+cboiteux2765@users.noreply.github.com>
2024-11-11 13:30:16 -0800

    Update events.ts

--- app/data/events.ts
+++ app/data/events.ts
@@ -26,6 +26,24 @@ const events = [
     imageUrl: "/images/blog2024-25/halloween-band-party.jpg"
   },
   {
+    title: "Undergrad Research Panel",
+    description: "We held an undergraduate research panel featuring Daniel Naylor, Pariya Akhiani, Jiaxin Su, and Henry McMahon! They shared very valuable advice for getting into research!",
+    date: "November 5, 2024",
+    imageUrl: "/images/blog2024-25/research-panel.jpg"
+  },
+  {
+    title: "Fall 2024 Data Science UCSB Career Fair",
+    description: "We hosted our 3rd annual Data Science UCSB Career Fair!",
+    date: "October 25, 2024",
+    imageUrl: "/images/careerfair24/2024-career-fair-logo.png",
+  },
+  {
+    title: "Data Science Band Party!!",
+    description: "We invited The Radar, Dawn Patrol, DMLW, and Orange Pit to play some fire music for us and bond with our spooky Halloween costumes!",
+    date: "October 18, 2024",
+    imageUrl: "/images/blog2024-25/halloween-band-party.jpg"
+  },
+  {
     title: "Intern Panel",
     description: "We hosted a panel featuring Tanay Biradar, Edizon Chang, Sarah Lew, Irena Wong, and Anu Maddali to talk about their internship experience!",
     date: "October 15, 2024",
