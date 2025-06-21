6defc849e79bf93e3fd8c8395645da8fa9e4e0a2
Kishan Goli <kishangoli@ucsb.edu>
2025-06-21 13:59:20 -0700

    landing page updates
    temporary logo displayed for 2 sec till animation is created

--- app/Main.tsx
+++ app/Main.tsx
@@ -1,32 +1,76 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import Image from "next/image";
+import LogoAnimation from "@/components/LogoAnimation";
 import HomepageEventsSection from "@/components/HomepageEventsSection";
 import SponsorsSection from "@/components/SponsorsSection";
 import events from "@/app/data/homepage-events";
 
 export default function Home() {
+  const [showAnimation, setShowAnimation] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowAnimation(false);
+    }, 2000); //(2 sec, change with gif length later)
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showAnimation) {
+    return <LogoAnimation />;
+  }
+
   return (
     <>
-      <div className="flex flex-col lg:flex-row items-center w-full">
+      <motion.div
+        className="flex flex-col lg:flex-row items-center w-full"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }} 
+        transition={{ duration: 0.5 }}
+      >
         <div className="flex-1">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <motion.h1
+            className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
             Welcome to Data Science UCSB!
-          </h1>
-          <p className="text-lg leading-7 text-gray-500 mt-2">
+          </motion.h1>
+          <motion.p
+            className="text-lg leading-7 text-gray-500 mt-2"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
             We prepare students for successful careers in data science +
-            analytics
-          </p>
+            analytics.
+          </motion.p>
         </div>
-        <div className="w-full lg:w-1/2 flex items-center justify-center mt-4 lg:mt-0">
+        <motion.div
+          className="w-full lg:w-1/2 flex items-center justify-center mt-4 lg:mt-0"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <Image
             src="/images/homepg.png"
             alt="Homepage Image"
             width={300}
             height={300}
           />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
 
-      <div className="text-center space-y-2 pb-8 pt-6 md:space-y-5">
+      <motion.div
+        className="text-center space-y-2 pb-8 pt-6 md:space-y-5"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
         <h1 className="text-2xl md:text-2xl lg:text-3xl font-extrabold leading-9 tracking-tight text-gray-900">
           Our Mission
         </h1>
@@ -37,7 +81,8 @@ export default function Home() {
           data science talent UCSB has to offer, as well as one that empowers
           students looking to earn this distinction.
         </p>
-      </div>
+      </motion.div>
+
       <div className="container mx-auto my-8">
         {events.map((event, index) => (
           <HomepageEventsSection
---
+++ components/LogoAnimation.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+export default function LogoAnimation() {
+  return (
+    <div className="logo-animation-container">
+      <div className="logo">
+        {/* replace with gif when created
+        <img src="/logo-animation.gif" alt="Logo Animation" className="logo-image" /> */}
+        <img src="/logo.png" alt="Logo Animation" className="logo-image" />
+      </div>
+    </div>
+  );
+}
