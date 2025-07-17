121eb90e719609abbddfcb400868e6d5c872d994
Kishan Goli <kishangoli@ucsb.edu>
2025-07-17 11:55:20 -0700

    Remove files

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,31 +1,31 @@
-import { Product } from '@/app/data/products'
-import ProductCard from './ProductCard'
-import { useCart } from './CartProvider'
-import { toast } from 'react-hot-toast'
+// import { Product } from '@/app/data/products'
+// import ProductCard from './ProductCard'
+// import { useCart } from './CartProvider'
+// import { toast } from 'react-hot-toast'
 
-interface ProductGridProps {
-  products: Product[]
-}
+// interface ProductGridProps {
+//   products: Product[]
+// }
 
-const ProductGrid: React.FC<ProductGridProps> = ({ products }) => {
-  const { dispatch } = useCart();
+// const ProductGrid: React.FC<ProductGridProps> = ({ products }) => {
+//   const { dispatch } = useCart();
   
-  const handleAddToCart = (product: Product) => {
-    dispatch({ type: 'ADD_TO_CART', payload: product })
-    toast.success(`${product.name} added to cart`)
-  }
+//   const handleAddToCart = (product: Product) => {
+//     dispatch({ type: 'ADD_TO_CART', payload: product })
+//     toast.success(`${product.name} added to cart`)
+//   }
 
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {products.map((product) => (
-        <ProductCard
-          key={product.id}
-          product={product}
-          onAddToCart={handleAddToCart}
-        />
-      ))}
-    </div>
-  )
-}
+//   return (
+//     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+//       {products.map((product) => (
+//         <ProductCard
+//           key={product.id}
+//           product={product}
+//           onAddToCart={handleAddToCart}
+//         />
+//       ))}
+//     </div>
+//   )
+// }
 
-export default ProductGrid;
+// export default ProductGrid;
