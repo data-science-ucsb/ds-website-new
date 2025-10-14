39bf156410ccbb8e924492360fed9c4981c99aff
Kishan Goli <kishangoli@ucsb.edu>
2025-10-13 18:58:54 -0700

    Add pulsating logo at launch
    Add pulsating logo at launch

--- components/LogoAnimation.tsx
+++ components/LogoAnimation.tsx
@@ -1,13 +1,18 @@
 "use client";
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function LogoAnimation() {
   return (
     <div className="logo-animation-container">
       <div className="logo">
-        {/* replace with gif when created
-        <img src="/logo-animation.gif" alt="Logo Animation" className="logo-image" /> */}
-        <img src="/logo.png" alt="Logo Animation" className="animate-spin-fast logo-image" />
+        <motion.img
+          src="/logo.png"
+          alt="Logo Animation"
+          className="logo-image"
+          animate={{ scale: [1, 1.2, 1] }}
+          transition={{ duration: 2, ease: "easeInOut", repeat: Infinity }}
+        />
       </div>
     </div>
   );
